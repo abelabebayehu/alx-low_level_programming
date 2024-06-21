d3cda978c1250aeb8f3ca93471ec1b65bfa4632f
abelabebayehu <abelperspectives@gmail.com>
2024-06-20 18:51:09 -0700

    edited puts.c to remove the script of bash

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-#!/bin/bash
 #include <studo.h>
 
 int main() {
-	puts("Programming is like building a multilingual puzzle\n";
+	puts("Programming is like building a multilingual puzzle\n");
 			return 0;
 			}
