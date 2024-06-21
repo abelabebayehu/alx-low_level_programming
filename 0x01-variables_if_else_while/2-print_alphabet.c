49f0f5b40ebc1707f5c62078371baf91a46339cd
abelabebayehu <abelperspectives@gmail.com>
2024-06-21 16:56:39 -0700

    added the 'orint alphabet' program

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*main -  Prints alphabets in lowercases
+*
+*Return: Always (Success)
+*
+*/
+int main (void)
+{
+		char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	return (0);
+}
