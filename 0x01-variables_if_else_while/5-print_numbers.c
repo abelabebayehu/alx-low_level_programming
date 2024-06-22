c5ae404c359b2b4f8e61c0376d8203f7a7664dc7
abelabebayehu <abelperspectives@gmail.com>
2024-06-21 17:21:40 -0700

    added the program that counts from 0 to 9 v2

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,19 @@
 #include  <stdio.h>
  /**
-  * main prints the numbers from 0 to 9 
-  *
+  * main prints the numbers from 0 to 9
+  *  *
   * Return: Always (Success)
   */
 int main(void)
 {
 	char n;
 
-	for (n = '0'; n < '10'; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
+	
+	putchar('\n');
+
+	return (0);
+}
