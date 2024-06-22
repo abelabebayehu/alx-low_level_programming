185a39e7efcddbbef7ea916f74a2b8e158205921
abelabebayehu <abelperspectives@gmail.com>
2024-06-21 17:12:45 -0700

    added a program that prints the alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the alphabets with an exception of the letters q and e
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
