3877ca334e0c0a419596d29369193f3818fff8fe
abelabebayehu <abelperspectives@gmail.com>
2024-06-27 02:10:14 +0300

    updated the code for last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,22 +3,23 @@
 /**
  * print_last_digit - Prints the last digit of a number
  *
- * @n: The number to be treated
+ * @n: The integer to be processed
  *
  * Convert the digit to its ASCII representation using 48
  *
  * Return: Value of the last digit of number
+ *
  */
 
 int print_last_digit(int n) 
 
-{
-	int last = n % 10;
-	
-	if (last < 0) 
 	{
-	last = -last;
-	}	
-	_putchar(last + 48);
-	 return last;
-}
+		int last = n % 10;
+	
+		if (last < 0) 
+		{
+		last = -last;
+		}	
+		_putchar(last + 48);
+		 return last;
+	}
