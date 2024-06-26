5456726a8d810a6cecdfa393393c76f19e2c8dd9
abelabebayehu <abelperspectives@gmail.com>
2024-06-26 04:52:22 +0300

    updated the c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
  *         */
 int main(void)
 {
-		_putchar('-');
+		_putchar('_');
 			_putchar('p');
 				_putchar('u');
 					_putchar('t');
@@ -17,5 +17,4 @@ int main(void)
 									_putchar('r');
 										_putchar('\n');
 
-												return (0);
-}
+
