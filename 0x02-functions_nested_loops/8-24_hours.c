3239f41efd10a333f3f3df5a6210745d98f7bb66
abelabebayehu <abelperspectives@gmail.com>
2024-06-27 04:47:39 +0300

    updated the 8-24_hours.c code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,17 +4,22 @@
  *
  * jack_bauer - Prints every minute of the day of Jack Bauer
  *
- * Return: ...
+ * Return: not set
  **/
 
 void jack_bauer(void)
 
 {
 	int i, j, k, l;
-	for (i = 0; i < 2; i++)
+
+	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 4; j++)
+		for (j = 0; j < 10; j++)
 		{
+			if (i == 2 && j == 3) 
+			{
+				break;
+			}
 			for (k = 0; k < 6; k++)
 			{
 				for (l = 0; l < 10; l++)
