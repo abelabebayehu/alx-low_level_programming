c60c47044da619d4ca3215de3f60d311fb165c58
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-06-27 20:23:25 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+  * more_numbers - print numbers from 0 to 14, 10 times
+  *
+  * Return:
+  */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+    {
+			_putchar(i + '0');
+    }
+    _putchar('\n');
+  }
+}
