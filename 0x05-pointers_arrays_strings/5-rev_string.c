735f314cbdd0034872a4b20d85dcc22c461fdac1
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-07-02 02:07:29 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,18 +10,15 @@
 
 void rev_string(char *s)
 {
-    int i, j;
-    char temp;
-
-    for (j = 0; s[j] != '\0'; j++)
-        ;
-
-    j--;
-
-    for (i = 0; i < j; i++, j--)
-    {
-        temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-    }
+int i, j;
+char temp;
+ for (j = 0; s[j] != '\0'; j++)
+ ;
+ j--;
+  for (i = 0; i < j; i++, j--)
+  {
+  temp = s[i];
+  s[i] = s[j];
+  s[j] = temp;
+  }
 }
