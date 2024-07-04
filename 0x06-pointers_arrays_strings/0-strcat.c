d995730c5830050c0ac09d0da572ec2c9bf43887
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-07-05 02:51:58 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,2 +1,29 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+  * _strcat - Concatenates two strings
+  * @dest: 1st string
+  * @src: 2nd string
+  *
+  * Return: A pointer to the resulting string dest
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
+}
