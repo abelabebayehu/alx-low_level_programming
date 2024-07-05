edf619ff171dd0c2fba65fa16d0997924de1aff1
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-07-05 03:15:58 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,2 +1,26 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+  * string_toupper - Changes lowercase to uppercase
+  * @p: The string to be modified
+  *
+  * Return: char var
+  */
+
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+
+		a++;
+	}
+
+	return (p);
+}
