748707578072b0cebc12121fe07af5aa63906135
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-07-05 05:51:51 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,20 +10,17 @@
 
 char *rot13(char *s)
 {
-    char *ptr;
-
-    for (ptr = s; *ptr != '\0'; ptr++) 
+    int i;
+    for (i = 0; s[i] != '\0'; i++) 
     {
-        if ((*ptr >= 'A' && *ptr <= 'Z') || (*ptr >= 'a' && *ptr <= 'z')) 
+        if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
         {
-            if ((*ptr >= 'A' && *ptr <= 'M') || (*ptr >= 'a' && *ptr <= 'm')) 
-            {
-                *ptr = *ptr + 13;
-            } else {
-                *ptr = *ptr - 13;
-            }
+            s[i] = s[i] + 13;
+        }
+        else if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
+        {
+            s[i] = s[i] - 13;
         }
     }
-
     return s;
 }
