50196fa8f10506dd9d6f04be7a4234a6169b4fa5
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-07-05 05:33:36 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 
 /**
-  * rot13 - converts strings by the rot13 code
+  * rot13 - converts strings by the rot13 encoding
   * @s: the string that is to be converted
   *
-  * Return: s
+  * Return: Pointer to the converted string @s
   */
 
 char *rot13(char *s)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++) 
-{
-if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
-{
-s[i] = s[i] + 13;
-}
-else
-{
-s[i] = s[i] - 13;
-}
-}
-return s;
+    char *ptr;
+
+    for (ptr = s; *ptr != '\0'; ptr++) {
+        if ((*ptr >= 'A' && *ptr <= 'Z') || (*ptr >= 'a' && *ptr <= 'z')) {
+            if ((*ptr >= 'A' && *ptr <= 'M') || (*ptr >= 'a' && *ptr <= 'm')) {
+                *ptr = *ptr + 13;
+            } else {
+                *ptr = *ptr - 13;
+            }
+        }
+    }
+
+    return s;
 }
