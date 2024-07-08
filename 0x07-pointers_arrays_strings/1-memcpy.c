e3d9183f3570428b2801115ef68bed72d76e3b35
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-07-08 16:54:02 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+  * _memcpy - copies n bytes
+  * @dest: destination memory area
+  * @src: source memory area
+  * @n: number of bytes to copy
+  *
+  * Return: copied memory
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+
+	return (dest);
+}
