209ffaa5fbbdd4f13726e426bd83044eede02ae1
abelabebayehu <abelperspectives@gmail.com>
2024-07-08 23:53:05 +0300

    updated 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
   * @s: source string
   * @c: character to find
   *
-  * Return: pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+  * Return: pointer to the first occurrence of the character
   */
 
 char *_strchr(char *s, char c)
