29ed0dba1f160aeb38b6fd6c2c1c0f0e7a3d1c28
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-08-06 19:48:08 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,15 +12,18 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	size_t count = 0;
+	unsigned int count = 0;
 
-	while (head != NULL)
+	if (head)
 	{
-		if (count == index)
-			return (head);
+		while (head)
+		{
+			if (count == index)
+				return (head);
 
-		head = head->next;
-		count++;
+			head = head->next;
+			count++;
+		}
 	}
 
 	return (NULL);
