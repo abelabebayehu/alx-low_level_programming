c34c85c425e3dcbd4bba45aed19841c40a55f71a
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-08-08 22:45:25 +0300

    Create 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * flip_bits - Counts the number of bits that differ between two numbers
+  * @n: The first number
+  * @m: The second number
+  *
+  * Return: The number of bits that need to be flipped to convert n to m
+  */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+    unsigned long int mask = n ^ m; 
+    unsigned int count = 0;
+
+    while (mask)
+    {
+        if (mask & 1)
+            count++;
+      
+        mask >>= 1;
+    }
+
+    return (count);
+}
